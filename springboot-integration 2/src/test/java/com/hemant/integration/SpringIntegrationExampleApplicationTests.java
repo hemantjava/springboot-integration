127025ee505a4952c3b9c526192fb26e11fd63c6
Hemant Kumar Sahu <hemantjava90@gmail.com>
2022-05-30 13:33:47 +0530

    Add files via upload

---
+++ springboot-integration 2/src/test/java/com/hemant/integration/SpringIntegrationExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hemant.integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIntegrationExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
